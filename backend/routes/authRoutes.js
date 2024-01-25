bf9ba9bf0dd94f85646683a65f5e6d40553b8666
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2024-01-25 18:57:39 +0000

    update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const AuthController = require('../controllers/authControllers');
 const router = express.Router();
 
-router.post('/register', AuthController.registerUser);
-router.post('/login', AuthController.loginUser);
+router.post('/', AuthController.registerUser);
+router.post('/', AuthController.loginUser);
 router.post('/forgotPassword', AuthController.forgotPassword);
 router.post('/resetPassword/:id/:token', AuthController.resetPassword);
 
