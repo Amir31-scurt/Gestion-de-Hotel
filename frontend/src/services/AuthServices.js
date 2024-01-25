9f4fff82fbf45636cc6a0581da9bd62b6c74028f
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2024-01-25 19:04:38 +0000

    uodate

--- frontend/src/services/AuthServices.js
+++ frontend/src/services/AuthServices.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
-const SERVER_URL = 'https://gestion-de-hotel.vercel.app/';
+const SERVER_URL = 'https://gestion-de-hotel.vercel.app/api'; // Adjusted to point to the /api directory
 
 const registerUser = (data) => {
-  return axios.post(SERVER_URL + '/', data);
+  return axios.post(`${SERVER_URL}/register`, data); // Removed the extra slash
 };
+
 const loginUser = (data) => {
-  return axios.post(SERVER_URL + '/', data);
+  return axios.post(`${SERVER_URL}/login`, data); // Removed the extra slash
 };
 
 const forgotPassword = (data) => {
-  return axios.post(SERVER_URL + '/forgotPassword', data);
+  return axios.post(`${SERVER_URL}/forgotPassword`, data); // Removed the extra slash
 };
+
 const resetPassword = ({ id, token, password }) => {
   return axios.post(`${SERVER_URL}/resetPassword/${id}/${token}`, { password });
 };
