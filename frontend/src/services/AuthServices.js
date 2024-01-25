2bcdf0eac27ea64e0793a639861b2709a3187083
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2024-01-25 22:36:41 +0000

    update

--- frontend/src/services/AuthServices.js
+++ frontend/src/services/AuthServices.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 const SERVER_URL = 'https://server-hotel-gest.onrender.com';
 
 const registerUser = (data) => {
-  return axios.post(SERVER_URL + '/register', data);
+  return axios.post(SERVER_URL + 'api/register', data);
 };
 const loginUser = (data) => {
-  return axios.post(SERVER_URL + '/login', data);
+  return axios.post(SERVER_URL + 'api/login', data);
 };
 
 const forgotPassword = (data) => {
-  return axios.post(SERVER_URL + '/forgotPassword', data);
+  return axios.post(SERVER_URL + 'api/forgotPassword', data);
 };
 const resetPassword = ({ id, token, password }) => {
   return axios.post(`${SERVER_URL}/resetPassword/${id}/${token}`, {
